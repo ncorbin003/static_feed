2ecb17aa89bad1939262b0c27a220d447752206f
ncorbin003 <ncorbin003@gmail.com>
2021-06-27 11:47:03 -0400

    removed extra fluff from receiver.js

--- static/js/receiver.js
+++ static/js/receiver.js
@@ -1,28 +1,15 @@
 $(document).ready(function(){
   let namespace = "/test";
-  //let video = document.querySelector("#videoElement");
-  let canvas = document.querySelector("#canvasElement");
-  let ctx = canvas.getContext('2d');
   photo = document.getElementById('photo');
-  var localMediaStream = null;
 
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
 
-  function sendSnapshot() {
-    //if (!localMediaStream) {
-      //return;
-    //}
+  socket.on('connect', function() {
+    console.log('Connected!');
+  });
 
-//    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, 300, 150);
-//
-//    let dataURL = canvas.toDataURL('image/jpeg');
-//    socket.emit('input image', dataURL);
-//
-//    socket.emit('output image')
-
+  socket.on('out-image-event',function(data){
     var img = new Image();
-    socket.on('out-image-event',function(data){
-
 
     img.src = dataURL//data.image_data
     photo.setAttribute('src', data.image_data);
@@ -30,27 +17,4 @@ $(document).ready(function(){
     });
 
 
-  }
-
-  socket.on('connect', function() {
-    console.log('Connected!');
-  });
-
-  var constraints = {
-    video: {
-      width: { min: 640 },
-      height: { min: 480 }
-    }
-  };
-
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    //video.srcObject = stream;
-    //localMediaStream = stream;
-
-    setInterval(function () {
-      sendSnapshot();
-    }, 50);
-  }).catch(function(error) {
-    console.log(error);
-  });
 });
