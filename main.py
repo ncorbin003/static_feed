49e74ab3d906d84e2bf4aee86db9e48b3795e56c
ncorbin003 <ncorbin003@gmail.com>
2021-07-02 10:15:46 -0400

    trying out vlc player

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ from flask_dance.contrib.github import make_github_blueprint, github
 from flask_sockets import Sockets
 from utils import base64_to_pil_image, pil_image_to_base64
 
-REDIS_URL = os.environ['REDIS_URL']
-REDIS_CHAN = 'chat'
-
 
 
 app = Flask(__name__, static_folder='static')
@@ -25,25 +22,7 @@ app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_S
 github_bp = make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
 
-socketio = SocketIO(app)
 camera = Camera()
-
-@socketio.on('input image', namespace='/test')
-def test_message(input):
-    input = input.split(",")[1]
-    camera.enqueue_input(input)
-    image_data = input # Do your magical Image processing here!!
-    #image_data = image_data.decode("utf-8")
-    image_data = "data:image/jpeg;base64," + image_data
-    print("OUTPUT " + image_data)
-    emit('out-image-event', {'image_data': image_data}, namespace='/test')
-    #camera.enqueue_input(base64_to_pil_image(input))
-
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    app.logger.info("client connected")
-
 
 @app.route("/login")
 def login():
@@ -51,7 +30,7 @@ def login():
         return redirect(url_for("github.login"))
     resp = github.get("/user")
     assert resp.ok
-    return render_template("success.html")
+    return render_template("streamer.html")
 
 
 def gen():
@@ -67,85 +46,21 @@ def gen():
 def index():
    return render_template("index.html")
 
-@app.route("/video_feed")
-def video_feed():
-   return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route("/video_feed")
+# def video_feed():
+#    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/streamer")
 def streamer():
-   return render_template("streamer.html")
+    return render_template("streamer.html")
 
-sockets = Sockets(app)
-redis = redis.from_url(REDIS_URL)
-
-
-
-class ChatBackend(object):
-    """Interface for registering and updating WebSocket clients."""
-
-    def __init__(self):
-        self.clients = list()
-        self.pubsub = redis.pubsub()
-        self.pubsub.subscribe(REDIS_CHAN)
-
-    def __iter_data(self):
-        for message in self.pubsub.listen():
-            data = message.get('data')
-            if message['type'] == 'message':
-                app.logger.info(u'Sending message: {}'.format(data))
-                yield data
-
-    def register(self, client):
-        """Register a WebSocket connection for Redis updates."""
-        self.clients.append(client)
-
-    def send(self, client, data):
-        """Send given data to the registered client.
-        Automatically discards invalid connections."""
-        try:
-            client.send(data)
-        except Exception:
-            self.clients.remove(client)
-
-    def run(self):
-        """Listens for new messages in Redis, and sends them to clients."""
-        for data in self.__iter_data():
-            for client in self.clients:
-                gevent.spawn(self.send, client, data)
-
-    def start(self):
-        """Maintains Redis subscription in the background."""
-        gevent.spawn(self.run)
-
-chats = ChatBackend()
-chats.start()
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@sockets.route('/submit')
-def inbox(ws):
-    """Receives incoming chat messages, inserts them into Redis."""
-    while not ws.closed:
-        # Sleep to prevent *constant* context-switches.
-        gevent.sleep(0.1)
-        message = ws.receive()
 
-        if message:
-            app.logger.info(u'Inserting message: {}'.format(message))
-            redis.publish(REDIS_CHAN, message)
-
-@sockets.route('/receive')
-def outbox(ws):
-    """Sends outgoing chat messages, via `ChatBackend`."""
-    chats.register(ws)
-
-    while not ws.closed:
-        # Context switch while `ChatBackend.start` is running in the background.
-        gevent.sleep(0.1)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=8000)
-   #app.run(debug = True,host="0.0.0.0",port=8000)
+    app.run(debug = True,host="0.0.0.0",port=8000)
