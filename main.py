a033d69c4208fcd353f31aef056b151181bc4280
ncorbin003 <71032579+ncorbin003@users.noreply.github.com>
2021-07-05 13:50:16 -0400

    made access to streamer

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 github_bp = make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
+app.register_blueprint(github_bp, url_prefix="/streamer")
 
 camera = Camera()
 
@@ -49,6 +50,10 @@ def index():
 
 @app.route("/streamer")
 def streamer():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
     return render_template("streamer.html")
 
 
