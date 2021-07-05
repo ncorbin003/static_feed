ac21442e3967db1f3ec9ad3152570ee5e0996ba0
ncorbin003 <71032579+ncorbin003@users.noreply.github.com>
2021-07-05 13:53:35 -0400

    changed it back

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 github_bp = make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
-app.register_blueprint(github_bp, url_prefix="/streamer")
-
 camera = Camera()
 
 @app.route("/login")
@@ -50,10 +48,6 @@ def index():
 
 @app.route("/streamer")
 def streamer():
-    if not github.authorized:
-        return redirect(url_for("github.login"))
-    resp = github.get("/user")
-    assert resp.ok
     return render_template("streamer.html")
 
 
