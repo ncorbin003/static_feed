7d027f955dfe8eee24e9369fbc7359a4d71055b8
ncorbin003 <71032579+ncorbin003@users.noreply.github.com>
2021-07-10 13:29:50 -0400

    trying out new IP address configuration

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 github_bp = make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
+myIP = os.environ("IP_URL")
 
 
 @app.route("/login")
@@ -17,7 +18,7 @@ def login():
         return redirect(url_for("github.login"))
     resp = github.get("/user")
     assert resp.ok
-    return render_template("streamer.html", IP_URL= os.environ.get("IP_URL"))
+    return render_template("streamer.html", IP_URL= myIP)
 
 
 @app.route("/")
